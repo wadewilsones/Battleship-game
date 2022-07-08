91fcef2a82ef785ae21afa5851565bd562392b2e
wadewilsones <wadewilsones@gmail.com>
2022-07-07 22:39:46 -0400

    Script was created

---
+++ battleship.js
@@ -0,0 +1,21 @@
+// Ship function
+
+const Ship = (n) => {
+    
+    let ships = {
+        shipLength: n,
+
+        hit: function(hitCoordinate){
+            //marks the coordinate as hit
+        },
+        
+        isSunk: function(){
+            //calculates is ship sinked based on length and hit marks
+        }
+    }
+
+}
+
+//Gameboard function
+
+//Player function
